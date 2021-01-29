7882500d753450f3a8dcbef7e4622bc4f2438830
Phebe S <75100910+phebems@users.noreply.github.com>
2021-01-29 23:32:24 +0000

    Store message components

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,4 +1,25 @@
 /* Every time a user runs a program, they should get a new, randomized output. 
-Random relatable lyrics. Outputs should be made up of at least three different pieces of data. 
+Random lyrics. Outputs should be made up of at least three different pieces of data. 
+
 */
-console.log('Mixed message generator');
+
+const openers = {
+    openerOne:'You can',
+    openerTwo:'A wise woman once said',
+    openerThree: 'Everybody knows'
+}
+const lyrics = {
+    setOne: ["Find me where the wild things are.", "Love me or hate me, I swear it won't make or break me.", "be the king but watch the queen conquer.", "Believe in your flyness, conquer your shyness."],
+    setTwo: ["I'm throwin' shade like it's sunny.", "Diamonds on me, what's the price? I'm not gettin' involved with the hype.", "Either you play the game or watch the game play you.", "Yeah, I'm in my bag, but I'm in his too.\nAnd that's why every time you see me, I got some new shoes.", "They say good things come to those who wait, so imma be at least an hour late."],
+    setThree: ["The night is still young and so are we.", "It ain’t no fun if the homies can’t have none.", "We ain’t picture perfect but we worth the picture still.", "This is our house. This is our rules.", "If you're a strong female, you don't need permission."]
+}
+
+
+switch(set) {
+    case openerOne:
+        break;
+    case openerTwo:
+        break;
+    case openerThree:
+        break;
+}
