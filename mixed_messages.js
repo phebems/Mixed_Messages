c954cdc38c1759793d0be498931d81c32c6aa090
Phebe S <75100910+phebems@users.noreply.github.com>
2021-01-29 21:22:20 +0000

    Change name

---
+++ mixed_messages.js
@@ -0,0 +1,4 @@
+/* Every time a user runs a program, they should get a new, randomized output. 
+Random relatable lyrics. Outputs should be made up of at least three different pieces of data. 
+*/
+console.log('Mixed message generator');
