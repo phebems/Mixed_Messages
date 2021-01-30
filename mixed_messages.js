3a66c395e2314708dfd55401029d152f6135fc40
Phebe S <75100910+phebems@users.noreply.github.com>
2021-01-30 03:15:26 +0000

    Added music notes

--- mixed_messages.js
+++ mixed_messages.js
@@ -4,13 +4,15 @@ Random lyrics.
 */
 let message;
 
+let musicNote = ` ♪ `;
+
 const openers = ['You can', 'A wise woman once said', 'Everybody knows']
 
 const lyrics = {
 
-    'You can': ["find me where the wild things are.", "love me or hate me, I swear it won't make or break me.", "be the king but watch the queen conquer.", "believe in your flyness, conquer your shyness."],
-    'A wise woman once said': ["I'm throwin' shade like it's sunny.", "diamonds on me, what's the price? I'm not gettin' involved with the hype.", "either you play the game or watch the game play you.", "yeah, I'm in my bag, but I'm in his too.\nAnd that's why every time you see me, I got some new shoes.", "they say good things come to those who wait, so imma be at least an hour late."],
-    'Everybody knows': ["the night is still young and so are we.", "it ain’t no fun if the homies can’t have none.", "we ain’t picture perfect but we worth the picture still.", "this is our house. this is our rules.", "if you're a strong female, you don't need permission."]
+    'You can': ["find me where the wild things are", "love me or hate me, I swear it won't make or break me", "be the king but watch the queen conquer", "believe in your flyness, conquer your shyness"],
+    'A wise woman once said': ["I'm throwin' shade like it's sunny", "diamonds on me, what's the price? I'm not gettin' involved with the hype", "either you play the game or watch the game play you", "yeah, I'm in my bag, but I'm in his too.\nAnd that's why every time you see me, I got some new shoes", "they say good things come to those who wait, so imma be at least an hour late"],
+    'Everybody knows': ["the night is still young and so are we", "it ain’t no fun if the homies can’t have none", "we ain’t picture perfect but we worth the picture still", "this is our house. this is our rules", "if you're a strong female, you don't need permission"]
 
 }
 // Takes in array of openers and outputs random opening quote from array
@@ -38,7 +40,7 @@ function getMessage() {
         
         if (opener === property) {
 
-            message = opener + ' ' + lyrics[opener][randIndex];
+            message =  opener + musicNote + lyrics[opener][randIndex] + musicNote;
 
         }
         
